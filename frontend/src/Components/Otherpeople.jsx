9a50e2ab976876641ef3fc6a67955955723fed5c
Abhishek Sachdeva <117009354+AbhishekSachdeva07@users.noreply.github.com>
2024-09-22 12:09:12 +0530

    Update Otherpeople.jsx

--- frontend/src/Components/Otherpeople.jsx
+++ frontend/src/Components/Otherpeople.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 const Otherpeople = ({data,userdata,setchatingdisplay,setanotheruserdata})=>{
     const [flightdata,setflightdata] = useState([]);
     const acceptedusers = async()=>{
-        const getdata = await axios.post(`http://localhost:5000/getotherusers/${userdata.userData._id}`,data,{
+        const getdata = await axios.post(`https://flights-lmrv.onrender.com/getotherusers/${userdata.userData._id}`,data,{
             withCredentials:true
         })
         if(getdata.data.otherflightusers === true){
